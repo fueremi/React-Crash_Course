199b5d710f741f15b74982124b36e0874c760972
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-05-15 17:06:08 +0700

    add Header and Button components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Header from './components/Header'
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello from React</h1>
+      <Header/>
     </div>
   );
 }
+
+// ! Class style
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
